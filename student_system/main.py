9864e9ce1687527adf9979a1d89a8eae3ba450ac
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-13 09:18:43 +0530

    Add Student with custom eerors

--- student_system/main.py
+++ student_system/main.py
@@ -9,5 +9,26 @@ def add_student():
         student_id=input("Enter Student ID (Format: S001): ")
         assert student_id.startswith("S"), "student ID must start with 'S'"
 
+        name=input("Enter Student name: ")
+        marks=int(input("Enter Marks (0-100): "))
+
+        if marks <0 or marks >100:
+            raise InvalidMarksError("Marks must be between 0 and 100.")
+        students=read_students()
+
+        #Prevent Duplicate IDs
+        for student in students:
+            if student["id"] == student_id:
+                print("Student ID already exists!")
+                return
         
+        add_student_to_file(student_id,name,marks)
+        print("Student added successfully")
+
+    except AssertionError as e:
+        print("Error:",e)
+    except InvalidMarksError as e:
+        print("Error:",e)
+    except ValueError:
+        print("Marks must be a valid number.")
 
